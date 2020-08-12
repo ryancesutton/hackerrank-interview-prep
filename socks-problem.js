5fd88a7f573c43d51c076c5f3c44d917a59acc29
Ry Sutton <ryancesutton@gmail.com>
2020-08-11 20:54:38 -0500

    complete sock merchant function

--- socks-problem.js
+++ socks-problem.js
@@ -3,9 +3,13 @@
 
 // Complete the sockMerchant function below.
 function sockMerchant(n, ar) {
-
-
-
-
+    let res = 0;
+    ar.sort();
+    for(let i = 0; i < n; i++){
+        if(ar[i] == ar[i+1]){
+            i++;
+            res++;
+        }
+    }
+    return res;
 }
-
