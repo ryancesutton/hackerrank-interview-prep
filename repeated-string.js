4f3fa9b298f14ef244f8e5dccdd30102261fda1b
Ry Sutton <ryancesutton@gmail.com>
2020-08-12 21:21:49 -0500

    beginnings of solution for repeated-string

---
+++ repeated-string.js
@@ -0,0 +1,15 @@
+function repeatedString(s, n) {
+
+    let count = 0;
+
+    for (let i = 0; i <= n; i++) {
+
+        if (s[i] === 'a') {
+            count++
+            {
+                return count;
+            }
+
+        }
+    }
+}
